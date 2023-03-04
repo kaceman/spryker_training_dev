db1f6bf775930a4469e8af0eacb2a889ceaf6737
Kacem EL AMRI <kaelamri@sqli.com>
2023-03-04 14:39:26 +0100

    added theme and changed title color

---
+++ src/Pyz/Shared/Twig/TwigConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Pyz\Shared\Twig;
+
+use Spryker\Shared\Twig\TwigConfig as SprykerTwigConfig;
+
+class TwigConfig extends SprykerTwigConfig
+{
+    public function getYvesThemeName(): string
+    {
+        return 'dunkirk';
+    }
+
+}
